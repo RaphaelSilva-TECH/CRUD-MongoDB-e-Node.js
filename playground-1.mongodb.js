9387325f1240380ecce27515b911573859ca9f8e
Rapha Silva <rafaelsilvamario@gmail.com>
2025-10-18 22:08:11 -0300

    chore: adiciona arquivos de configuração do npm

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -10,10 +10,10 @@
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
 // Select the database to use.
-use('mongodbVSCodePlaygroundDB');
+use('meubanco');
 
-// Insert a few documents into the sales collection.
-db.getCollection('sales').insertMany([
+// Insert a few documents into the itens collection.
+db.getCollection('itens').insertMany([
   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
@@ -25,19 +25,19 @@ db.getCollection('sales').insertMany([
 ]);
 
 // Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
+const itensOnApril4th = db.getCollection('itens').find({
   date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
 }).count();
 
 // Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
+console.log(`${itensOnApril4th} itens occurred in 2014.`);
 
 // Here we run an aggregation and open a cursor to the results.
 // Use '.toArray()' to exhaust the cursor to return the whole result set.
 // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
+db.getCollection('itens').aggregate([
+  // Find all of the itens that occurred in 2014.
   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
-  // Group the total sales for each product.
+  // Group the total itens for each product.
   { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
 ]);
